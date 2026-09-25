d25b9573472e151ab7fc9cb98a981f34edab4bbd
smit malaviya <smitmalaviya80@gmail.com>
2026-09-25 23:01:56 +0530

    Connect frontend to deployed backend

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = "https://weathergpt-backend-t8x3.onrender.com";
 
 // DOM Elements
 const healthPill = document.getElementById('healthPill');
